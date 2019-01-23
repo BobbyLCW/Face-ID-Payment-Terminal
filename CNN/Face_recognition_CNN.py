f100c23773c06f21c3183997d4b7ccd5280a62b0
Bobby Lee <mfbobbylee@gmail.com>
2019-01-23 22:04:05 +0800

    reconstruct folder structure

---
+++ CNN/Face_recognition_CNN.py
@@ -0,0 +1,101 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.layers.convolutional import ZeroPadding2D
+from keras.preprocessing.image import img_to_array, load_img
+from keras import backend as K
+import numpy as np
+import tensorflow as tf
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]=""
+
+# dimensions of our images.
+img_width, img_height = 150, 150
+
+train_data_dir = 'data/train'
+validation_data_dir = 'data/validation'
+nb_train_samples = 3210
+nb_validation_samples = 963
+epochs = 40
+batch_size = 30
+number_of_class = 3
+
+if K.image_data_format() == 'channels_first':
+    input_shape = (1, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 1)
+
+model = Sequential()
+#model.add(ZeroPadding2D((1,1),input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu', input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2,2)))
+
+#model.add(ZeroPadding2D((1,1)))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2,2)))
+
+#model.add(ZeroPadding2D((1,1)))
+model.add(Conv2D(256, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2,2)))
+
+#model.add(ZeroPadding2D((1,1)))
+model.add(Conv2D(512, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(number_of_class, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+# this is the augmentation configuration we will use for training
+train_datagen = ImageDataGenerator(rescale=1. / 255, rotation_range=20,
+                               width_shift_range=0.1,
+                               height_shift_range=0.1,
+                               shear_range=0.01,
+                               horizontal_flip=True,
+                               vertical_flip=True,
+                               brightness_range=[0.5, 1.5])
+
+# this is the augmentation configuration we will use for testing:
+# only rescaling
+test_datagen = ImageDataGenerator(rescale=1. / 255, rotation_range=20,
+                               width_shift_range=0.1,
+                               height_shift_range=0.1,
+                               shear_range=0.01,
+                               horizontal_flip=True,
+                               vertical_flip=True,
+                               brightness_range=[0.5, 1.5])
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    color_mode = 'grayscale',
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='categorical')
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_dir,
+    color_mode = 'grayscale',
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='categorical')
+	
+print("\nvalidation: ")
+print(validation_generator.class_indices)
+
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=nb_validation_samples // batch_size)
+
+model.save('FaceRecModel.h5')
